580e990bf64a6e3f52e9a5d4cc7007c774efbb90
sonnd004 <76640476+son2104a5@users.noreply.github.com>
2024-01-19 09:50:44 +0700

    Add files via upload

--- Ss30/bai5.js
+++ Ss30/bai5.js
@@ -1,14 +1,17 @@
-function uppercase(){
-    let string = 'Hello World';
-    let x;
-    for(let i=0;i<=string.length;i++){
-        if(i==' '||i==undefined){
-            x = string.charAt(i-1);
-            console.log(x);
-        }
-        
-    }
-    // console.log(string);
+function capitalizeLastLetter(inputString) {
+    const words = inputString.split(' ');
+
+    const capitalizedWords = words.map(word => {
+        const lastCharIndex = word.length - 1;
+        return word.slice(0, lastCharIndex) + word.charAt(lastCharIndex).toUpperCase();
+    });
+
+    const resultString = capitalizedWords.join(' ');
+
+    return resultString;
 }
 
-uppercase();
+const userInput = 'Hello world';
+
+const result = capitalizeLastLetter(userInput);
+console.log(result);
